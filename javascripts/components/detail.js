6bfd57fa888631a36dde64bd1d07cc1bd8360765
Robert Rice <rrice1291@gmail.com>
2018-10-02 18:59:43 -0500

    added close button

--- javascripts/components/detail.js
+++ javascripts/components/detail.js
@@ -1,9 +1,17 @@
 import{printToDom} from '../helpers/util.js'
+import{charactersBuilder} from './characters.js'
 
+const closeButtonEvent = () => {
+    const closeButton = document.getElementById('close');
+    closeButton.addEventListener('click',charactersBuilder);
+}
 
 const detailsBuilder = (characters) => {
 let newString = '';
 newString += `<div class="col-6 offset-md-3">`
+newString += `<div class="row">`
+newString += `<button class="btn btn-danger" id="close">x</button>`;
+newString += `</div>`
 newString += `<div class="row">`
 newString += `<div class="col">`
 newString += `<img src="${characters.imageUrl}"/>`
@@ -16,6 +24,7 @@ newString += `</div>`
 newString += `</div>`
 
 printToDom(newString);
+closeButtonEvent();
 };
 
 
